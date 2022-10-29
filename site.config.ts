3b125d6024624d3626863e9e86fb0e574a368814
yema-29129 <67180306+yema-29129@users.noreply.github.com>
2022-10-29 18:09:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e72333bce72f4df197256f915593b844',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '天马黑珍珠',
+  domain: 'https://guanwaiyema.notion.site/e72333bce72f4df197256f915593b844',
+  author: '关外野马&马若夕',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
